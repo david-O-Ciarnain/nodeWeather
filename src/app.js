fbf543563da4a3219f67e9076e3d751c5e10e68c
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2023-01-09 17:44:06 +0100

    change from static html to hbs template engine

--- src/app.js
+++ src/app.js
@@ -6,7 +6,29 @@ const PORT = 3000;
 
 const publicPath = path.join(__dirname, "../public/");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Sven",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Sven",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "HELP PAGE",
+    message: "This should be helpful or is it? ;)",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.status(200).send({
