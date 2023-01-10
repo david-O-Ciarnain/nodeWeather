c6b0abb8acf735f47a626443703b209418cc20dc
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2023-01-10 13:48:24 +0100

    display weather data and icon in browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,39 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-let messageOne = document.querySelector("#message-1");
-let messageTwo = document.querySelector("#message-2");
+const bodyElemnt = document.querySelector("body");
+let locations = document.querySelector("#message-4");
+let descriptions = document.querySelector("#message-1");
+let temperatures = document.querySelector("#message-2");
+let feelslikes = document.querySelector("#message-3");
+let weatherIcon = document.createElement("img");
 
-messageOne.textContent = "";
-messageTwo.textContent = "";
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const location = search.value;
 
   const url = `http://localhost:3000/weather?address=${location}`;
+
   fetch(url).then((res) =>
     res.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
-        messageOne.textContent = data.error;
+        descriptions.textContent = data.error;
+
+        //resets elements
+        temperatures.textContent = "";
+        feelslikes.textContent = "";
+        locations.textContent = "";
+        weatherIcon.src = "";
       } else {
         const { description, temperature, feelslike } =
           data["forecast"]["weather"];
-        console.log(data);
-        console.log(description);
-        messageOne.textContent = description;
+    
+        weatherIcon.src = data["forecast"].weatherIcons;
+        
+        bodyElemnt.appendChild(weatherIcon);
+        locations.textContent = data.location;
+        descriptions.textContent = description;
+        temperatures.textContent = temperature;
+        feelslikes.textContent = feelslike;
       }
     })
   );
